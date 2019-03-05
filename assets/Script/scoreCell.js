9bef019cdd63fd9e9b4c14ea89922e63466877ab
youyou <leigned@outlook.com>
2019-03-05 08:07:34 +0800

    修改分数

--- assets/Script/scoreCell.js
+++ assets/Script/scoreCell.js
@@ -4,7 +4,6 @@ cc.Class({
   properties: {
     label: cc.Label,
     particle: cc.ParticleSystem,
-
   },
 
   // LIFE-CYCLE CALLBACKS:
@@ -17,14 +16,22 @@ cc.Class({
     this.label.string = num
     this.particle.resetSystem()
     this.node.scale = 1
+    this.label.node.x = 0
+    this.label.node.y = 0
+    this.label.node.scale = 1
     let action1 = cc.scaleTo(0.1, 1.2, 1.2)
     let action2 = cc.moveBy(0.1, 0, 30)
     let action3 = cc.moveTo(0.3, 0, 500)
     let action4 = cc.scaleTo(0.3, 0, 0)
-    let seq = cc.sequence(cc.spawn(action1, action2), cc.moveBy(0.2, 0, 0), cc.spawn(action3, action4), cc.callFunc(() => {
-      s.scorePool.put(this.node)
+
+    let seq = cc.sequence(cc.spawn(action1, action2), cc.moveBy(0.3, 0, 0), cc.callFunc(() => {
+      let seq2 = cc.sequence(cc.spawn(action3, action4), cc.callFunc(() => {
+        s.scorePool.put(this.node)
+      }, this))
+      this.node.runAction(seq2)
     }, this))
-    this.node.runAction(seq)
+    this.label.node.runAction(seq)
+
   }
 
   // update (dt) {},
